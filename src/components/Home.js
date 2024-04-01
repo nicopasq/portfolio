0332ed2c762550cce37268a92fd2b3eeb69ed5c4
nicopasq <90681300+nicopasq@users.noreply.github.com>
2024-03-31 23:45:38 -0600

    Added plan for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import React from "react";
 export default function Home(){
     return (
         <div>
+            {/* SCROLLING BAR (NICO PASQUARIELLO: FRONTEND DEV) */}
+            {/* Brief about blurb */}
+            {/* Professional Photo */}
             MAIN PAGE CONTENT
         </div>
     )
