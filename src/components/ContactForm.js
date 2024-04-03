4faf2803dfb96a706c8609e0ec59d11234ce4dae
Nico <nicopasquariello@gmail.com>
2024-04-03 15:11:15 -0600

    added Contact me form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,11 +1,38 @@
+import { Button, Container, TextField, Typography } from "@mui/material";
 import React from "react";
 
 export default function ContactForm(){
     return (
         <div className="contactFormContainer">
-            <h1>Contact Me</h1>
+            <h1 style={{position:"relative", bottom:'4vh'}}>Contact Me</h1>
             <form className="contactForm">
-                <h4>Name </h4>
+
+                <div className="inputContainer">
+                    <h6 className="inputTitle">Name (required)</h6>
+                    <label className="nameLabel">First Name</label>
+                        <input className="nameInput" />
+
+                    <label className="nameLabel">Last Name</label>
+                        <input className="nameInput"/>
+                </div>
+
+                <div className="inputContainer">
+                <h6 className="inputTitle">Email (required)</h6>
+                <input className="contactInput"/>
+                </div>
+
+                <div className="inputContainer">
+                <h6 className="inputTitle">Subject (required)</h6>
+                <input className="contactInput"/>
+                </div>
+
+                <div className="inputContainer">
+                <h6 className="inputTitle">Message (required)</h6>
+                <textarea className="messageInput"/>
+                </div>
+
+                <Button variant="contained" id="submitMessage" type="submit">Send</Button>
+
             </form>
         </div>
     )
