20b993fb31cd9479684996948c220a927441f07d
Nico <nicopasquariello@gmail.com>
2024-04-03 15:17:24 -0600

    contact me form is now controlled.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,34 +1,51 @@
 import { Button, Container, TextField, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 export default function ContactForm(){
+    const [formInput, setFormInput] = useState({
+        firstName:'',
+        lastName:'',
+        email:'',
+        subject:'',
+        message:''
+    })
+
+    function submit(e){
+        e.preventDefault();
+        console.log(formInput)
+    }
+
+    function updateInput(e){
+        setFormInput({...formInput, [e.target.name] : e.target.value})
+    }
+
     return (
         <div className="contactFormContainer">
             <h1 style={{position:"relative", bottom:'4vh'}}>Contact Me</h1>
-            <form className="contactForm">
+            <form className="contactForm" onSubmit={(e) => submit(e)}>
 
                 <div className="inputContainer">
                     <h6 className="inputTitle">Name (required)</h6>
                     <label className="nameLabel">First Name</label>
-                        <input className="nameInput" />
+                        <input className="nameInput" name='firstName' value={formInput.firstName} onChange={(e) => updateInput(e)} />
 
                     <label className="nameLabel">Last Name</label>
-                        <input className="nameInput"/>
+                        <input className="nameInput" name='lastName' value={formInput.lastName} onChange={(e) => updateInput(e)}/>
                 </div>
 
                 <div className="inputContainer">
                 <h6 className="inputTitle">Email (required)</h6>
-                <input className="contactInput"/>
+                <input className="contactInput" name='email' value={formInput.email} onChange={(e) => updateInput(e)}/>
                 </div>
 
                 <div className="inputContainer">
                 <h6 className="inputTitle">Subject (required)</h6>
-                <input className="contactInput"/>
+                <input className="contactInput" name='subject' value={formInput.subject} onChange={(e) => updateInput(e)}/>
                 </div>
 
                 <div className="inputContainer">
                 <h6 className="inputTitle">Message (required)</h6>
-                <textarea className="messageInput"/>
+                <textarea className="messageInput" name='message' value={formInput.message} onChange={(e) => updateInput(e)}/>
                 </div>
 
                 <Button variant="contained" id="submitMessage" type="submit">Send</Button>
