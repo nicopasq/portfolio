f21a5d9e8d875e92741287b8ac01a0a27b681624
nicopasq <90681300+nicopasq@users.noreply.github.com>
2024-04-08 08:39:24 -0600

    Added content to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,25 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import gitHub_logo from '../project_images/githubLogo.png'
+import linkedIn_logo from '../project_images/linkedInLogo .png'
 
 export default function Footer(){
 
     return (
-        <div id="footer">
+        <div className="footer">
             <h3 style={{marginLeft:'10px'}}>PASQMEDIA</h3>
-            <div id="contactMe">
-                <h3>Gmail</h3>
+            <div className="contactMe">
+                <h3 >Gmail</h3>
                 <h4>nicopasquariello@gmail.com</h4>
+            </div>
+            <div className="socials">
+                <h3>Socials</h3>
+                <a href="https://github.com/nicopasq">
+                    <img id="github" src={gitHub_logo} alt="github"/>
+                </a>
+                <a href="https://www.linkedin.com/in/nicohpasq/">
+                    <img id="linkedIn" src={linkedIn_logo} alt="LinkedIn"/>
+                </a>
             </div>
         </div>
     )
